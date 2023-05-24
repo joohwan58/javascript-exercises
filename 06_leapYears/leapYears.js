edec7438d100338ce8f9e6b01598f3c2dc7194f7
Kim Joohwan <kim.joohwan58@gmail.com>
2023-05-24 14:08:27 +0900

    excercise 6 complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result = false;
+    if (year % 4 === 0) {
+        result = true;
+    }
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            result = true;
+        } else {
+            result = false;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
