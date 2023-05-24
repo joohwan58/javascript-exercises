2f51ef60154abe63f9308b3e1c2b71c06201ef88
Kim Joohwan <kim.joohwan58@gmail.com>
2023-05-24 13:37:05 +0900

    exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...check) {
+    let newArray = [];
+    array.forEach((item) => {
+        let checked = false;
+        check.forEach((x) => {
+            if (x === item) {
+                checked = true;
+            }
+        });
+        if (checked) {
+            /*remove from array*/
+        } else {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
