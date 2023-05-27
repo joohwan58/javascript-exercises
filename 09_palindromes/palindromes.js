29230ccf4e722617b04f992e20a0c5808f92f269
Kim Joohwan <kim.joohwan58@gmail.com>
2023-05-27 21:15:05 +0900

    progress on exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    const formattedStringAsArray = string.replace(punctuationRegex, '').toLowerCase().split("");
+    // return formattedString === formattedString.split("").reverse().join("");
+    // return formattedString.split("").reverse().join("");
 };
 
 // Do not edit below this line
