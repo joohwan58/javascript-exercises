11a7e87530c002d494adb85fd5dfc3b0a5337ad8
Kim Joohwan <kim.joohwan58@gmail.com>
2023-05-29 15:18:49 +0900

    exercise 9 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,11 @@
 const palindromes = function (string) {
     const punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
-    const formattedStringAsArray = string.replace(punctuationRegex, '').toLowerCase().split("");
+    const formattedStringAsArray = string.replace(punctuationRegex, '').toLowerCase().split("").filter((item) => item !== " ");
     // return formattedString === formattedString.split("").reverse().join("");
     // return formattedString.split("").reverse().join("");
+    const originalString = formattedStringAsArray.join("");
+    const reversedString = formattedStringAsArray.reverse().join("");
+    return originalString == reversedString;
 };
 
 // Do not edit below this line
