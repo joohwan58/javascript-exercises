3e4b283bd115bbb28fdd0e3d47410f6cb39fe19a
Kim Joohwan <kim.joohwan58@gmail.com>
2023-05-29 16:26:05 +0900

    exercise 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    function getAge(item) {
+        if ('yearOfDeath' in item) {
+            return item.yearOfDeath - item.yearOfBirth;
+        } else {
+            return new Date().getFullYear() - item.yearOfBirth;
+        }
+    }
+    return array.reduce((oldest, person) => {
+        if (getAge(person) > getAge(oldest)) {
+            return person;
+        } else {
+            return oldest;
+        }
+    }, { name: "placeholder", yearOfBirth: 2011, yearOfDeath: 2011});
+    
+    
 };
 
 // Do not edit below this line
