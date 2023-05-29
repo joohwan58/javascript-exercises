2082a0293c471aef602567caf1d971a9d070d3d0
Kim Joohwan <kim.joohwan58@gmail.com>
2023-05-29 16:07:37 +0900

    exercise 11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map((item) => {
+        return item.title;
+    });
 };
 
 // Do not edit below this line
