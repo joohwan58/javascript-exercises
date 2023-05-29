f9ce273833c8a3d8cdd3beca95631f579cf6220e
Kim Joohwan <kim.joohwan58@gmail.com>
2023-05-29 16:05:01 +0900

    exercise 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num;
+    if (typeof n == 'string') {
+        num = parseInt(n);
+    } else {
+        num = n;
+    }
+    if (num <= 0) {
+        return 'OOPS';
+    }
+    if (num === 1 || num === 2) {
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 };
 
 // Do not edit below this line
